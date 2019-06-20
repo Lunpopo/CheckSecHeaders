1942dcebb60885305a9b9819cc42bb20b737de1a
Lunpopo <popolun@Lunpopo.local>
2019-06-20 13:24:22 +0800

    modifying construction of main function that modify getopt to argparse function

--- CheckSecHeaders.py
+++ CheckSecHeaders.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python2
 # encoding: utf8
-import getopt
+from argparse import ArgumentParser
+from argparse import SUPPRESS
 import requests
 import sys
 import re
+from termcolor import colored
+import subprocess
 
-# 测试的机型为 Windows 7 专业版，测试的 url 为 https://www.baidu.com
 # test Custom header with Windows 7 professor for https://www.baidu.com
 custom_headers = {
-    'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0',
     'Accept': '*/*',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
     'Accept-Encoding': 'gzip, deflate',
@@ -17,7 +19,6 @@ custom_headers = {
     'Connection': 'close'
 }
 
-# 服务器端响应应该被设置的安全头如下
 # security headers that should be setted up for
 security_headers = {
     'X-XSS-Protection': False,
@@ -31,76 +32,36 @@ security_headers = {
 
 
 def banner():
-    print """
+    print colored("""
  .--..           .    .-.         .   .             .
 :    |           |   (   )        |   |             |
 |    |--. .-. .-.|.-. `-.  .-. .-.|---| .-. .-.  .-.| .-. .--..--.
 :    |  |(.-'(   |-.'(   )(.-'(   |   |(.-'(   )(   |(.-' |   `--.
  `--''  `-`--'`-''  `-`-'  `--'`-''   ' `--'`-'`-`-'`-`--''   `--'
-
-[*] Check Security Headers
-[*] 这是检查服务端是否设置安全头的小脚本
-[*] This is check if server has setted up security headers small script
-[*] 我爱中国！
-[*] I Love China!
-------------------------------------------------------------------------
-"""
+    """, 'cyan')
 
 
-ENGLISH_BUTTON = False
+def argument_parse():
+    parse = ArgumentParser(description='This is check if server has setted up security headers small script',
+                           add_help=False)
 
+    helper = parse.add_argument_group('Help')
+    helper.add_argument('-h', '--help', action='help', default=SUPPRESS, help="Show this help message and exit")
 
-def english_helper():
-    """
-    This function aim to show detail English helper for this script
+    mandatory = parse.add_argument_group('Mandatory Arguments')
+    mandatory.add_argument('-u', '--url', dest='target_url', metavar='URL', help="Scan target URL")
 
-    :return:
-    """
+    optional_argument = parse.add_argument_group('Optional Arguments')
+    optional_argument.add_argument('-c', '--cookie', dest='cookies', metavar='COOKIES',
+                                   help="Set cookies for URL headers, for example: "
+                                        "--cookies='PS=1560911095; BR_RPN=123253;'")
+    optional_argument.add_argument('-d', '--disable_ssl_check', dest='disable_ssl', action='store_true',
+                                   help="disable SSL/TLS certificate")
+    optional_argument.add_argument('--proxy', dest='target_proxy', metavar="PROXY_URL",
+                                   help="Set proxy to request url for example: --proxy='http://127.0.0.1'")
 
-    banner()
-
-    print
-    print '-h                      Show Help'
-    print '--help'
-    print
-    print '-e                      Show English Help'
-    print '--english_help'
-    print
-    print '-c COOKIES              Set Cookies for URL Headers'
-    print '--cookie="COOKIES"'
-    print
-    print '-d                      disable SSL/TLS certificate'
-    print '--disable_ssl_check'
-    print
-    print '--proxy="PROXY_URL"     Set proxy to request url'
-    print '                        for example: --proxy="http://127.0.0.1"'
-
-
-def usage():
-    """
-    这是一个使用方法, 包含所有的 options 和说明
-
-    :return: None
-    """
-
-    # banner()
-
-    print
-    print '-h                      展示此脚本的帮助信息'
-    print '--help'
-    print
-    print '-e                      show English help'
-    print '--english_help'
-    print
-    print '-c COOKIES              设置 cookies 到 headers 中'
-    print '--cookie="COOKIES"'
-    print
-    print '-d                      关闭 SSL/TLS 证书验证'
-    print '--disable_ssl'
-    print
-    print '--proxy="PROXY_URL"     设置一个代理, 例如: http://127.0.0.1'
-
-    sys.exit(0)
+    opts = parse.parse_args()
+    return opts
 
 
 def normalization_url(url):
@@ -145,22 +106,16 @@ def send_request(url, **kwargs):
         proxy, 如果指定了 -p --proxy 选项
     :return:
     """
-
-    custom_cookie = kwargs.get('cookie', None)
-    # disable_ssl 暂时不用
-    # custom_disable_ssl = kwargs.get('disable_ssl', False)
     custom_proxy = kwargs.get('proxy', None)
 
+    # disable_ssl 暂时不用
+    # custom_disable_ssl = kwargs.get('disable_ssl', False)
     # 暂时不要, 因为我没看见报错
     # if custom_disable_ssl:
     #     context = ssl.create_default_context()
     #     context.check_hostname = False
     #     context.verify_mode = ssl.CERT_NONE
     #     context = ssl._create_unverified_context()
-
-    # 如果指定了 -c --cookie, 就添加进 custom_headers 中
-    if custom_cookie is not None:
-        custom_headers['Cookie'] = custom_cookie
 
     # 指定了 proxy
     if custom_proxy is not None:
@@ -182,39 +137,37 @@ def send_request(url, **kwargs):
     return response
 
 
-def format_color(string, type):
+def format_color(string, format_type):
     """
     格式化颜色的
 
     :param string: 文本
-    :param type: 要展示的颜色类型
+    :param format_type: 要展示的颜色类型
     :return:
     """
+    if format_type == 'ok':
+        return colored(string, 'green')
+    elif format_type == 'error':
+        return colored(string, 'red')
+    elif format_type == 'warning':
+        return colored(string, 'yellow')
+    else:
+        return colored(string, 'red')
 
-    color = {
-        'error': '\033[0;31;31m {} \033[0m',
-        'ok': '\033[1;32m {} \033[0m',
-        'warning': '\033[4;33m {} \033[0m'
-    }
 
-    return color.get(type).format(string)
-
-
-def display_sec_header(security_headers):
+def display_sec_header(sec_headers):
     """
     仅仅只做输出展示
 
-    :param security_headers: 输入安全头字典, dic 类型
+    :param sec_headers: 输入安全头字典, dic 类型
     :return: None
     """
-
-    banner()
 
     _ = 28*'*'
     print "{}{}{}".format(_, "服务器安全头展示", _)
     print
 
-    for sec_header in security_headers:
+    for sec_header in sec_headers:
         if security_headers[sec_header]:
             print "{}: {}".format(sec_header, format_color(security_headers[sec_header], 'ok'))
         else:
@@ -224,61 +177,52 @@ def display_sec_header(security_headers):
     print
     print "{}{}{}".format(_, "服务器安全头展示", _)
 
+    return None
+
 
 def main():
     """
     接收所有的命令行参数, 然后根据 options 的参数配置去发送自定义请求
 
-    :return:
+    :return: None
     """
-
-    custom_url = None
-    custom_port = 80
-    custom_cookie = None
     disable_ssl = False
     proxy = None
 
-    try:
-        args, opts = getopt.getopt(sys.argv[1:], 'heu:c:d', ['help', 'url=', 'cookie=', 'proxy='])
-    except getopt.GetoptError:
-        usage()
+    if not len(sys.argv[1:]):
+        popen = subprocess.Popen('python {} -h'.format(sys.argv[0]), shell=True, stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, code = popen.communicate()
+        print output
+    else:
+        banner()
 
-    for o, a in args:
-        if ('-h' or '--help') in o:
-            usage()
-        elif ('-e' or '--english_help') in o:
-            english_helper()
-            # 打开全局 English 开关
-            ENGLISH_BUTTON = True
-        elif ('-u' or '--url') in o:
-            # 格式化 url, 格式化函数可以做成一个 格式化 类, 然后不同的格式化方法
-            a = normalization_url(a)
-            custom_url = a
-        elif ('-c' or '--cookie') in o:
-            # 想发送自定义cookie, 接收把它加入 custom_headers 中, 最后去发送
-            # 这里应该判断获取的 cookie 的格式
-            custom_cookie = a
-        elif ('-d' or '--disable_ssl') in o:
+        opts = argument_parse()
+        if opts.cookies:
+            # 想发送自定义cookie, 接收把它加入 custom_headers
+            custom_headers['Cookie'] = opts.cookies
+
+        if opts.disable_ssl:
             disable_ssl = True
-        elif '--proxy' in o:
+
+        if opts.target_proxy:
             # 这里应该判断 --proxy 的输入的格式
-            proxy = a
-        else:
-            usage()
+            proxy = opts.target_proxy
 
-    # 发送请求, 获取 响应
-    response = send_request(custom_url, cookie=custom_cookie, disable_ssl=disable_ssl, proxy=proxy)
+        if opts.target_url:
+            normal_url = normalization_url(opts.target_url)
+            # 发送请求, 获取 响应
+            response = send_request(normal_url, disable_ssl=disable_ssl, proxy=proxy)
 
-    print
+        for sec_header in security_headers:
+            if sec_header in response.headers:
+                security_headers[sec_header] = response.headers[sec_header]
 
-    for sec_header in security_headers:
-        if sec_header in response.headers:
-            security_headers[sec_header] = response.headers[sec_header]
+        # 展示安全头哪些设置了, 哪些没有设置
+        display_sec_header(security_headers)
 
-    # 展示安全头哪些设置了, 哪些没有设置
-    display_sec_header(security_headers)
+        return None
 
 
 if __name__ == '__main__':
     main()
-
